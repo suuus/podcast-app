9b6936725dfffa6f15b813496ca86dffacacbe28
Isaac Levin <8878502+isaacrlevin@users.noreply.github.com>
2023-10-04 19:59:09 +0000

    Add App

---
+++ src/Podcast/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Podcast.Pages;
+
+
+    public class Index : PageModel
+    {
+        //private readonly PodcastService _podcastService;
+
+        //public Show[]? FeaturedShows { get; set; }
+
+        public Index()
+        {
+            //_podcastService = podcastService;
+        }
+
+        public async Task OnGet()
+        {
+            // var shows = await _podcastService.GetShows(50, null);
+            // FeaturedShows = shows?.Where(s => s.IsFeatured).ToArray();
+        }
+    }
+
